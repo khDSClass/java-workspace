8d436ec260a55ebb3515e218dceae8b7d221b556
khDSClass <next007@iei.or.kr>
2024-11-20 14:29:40 +0900

    학생관리 프로그램 v2 - 2

--- JAVA_StudentMgr2/src/kr/co/iei/student/controller/StudentController.java
+++ JAVA_StudentMgr2/src/kr/co/iei/student/controller/StudentController.java
@@ -19,7 +19,23 @@ public class StudentController {
 		students = new Student[10];
 		sc = new Scanner(System.in);
 		index = 0;
-	}
+		
+		Student s1 = new Student();
+		s1.setName("학생1");
+		s1.setAge(20);
+		s1.setAddr("서울");
+		students[index] = s1;
+		index++;
+		
+		Student s2 = new Student("학생2", 21, "경기도");
+		students[index] = s2;
+		index++;
+		
+		students[index] = new Student("학생3", 22, "인천");
+		index++;
+		
+		students[index++] = new Student("학생4",23,"부천");
+	} 
 	//메인메뉴를 진행하는 메소드
 	public void main() {
 		while(true) {
@@ -37,12 +53,16 @@ public class StudentController {
 				insertStudent();
 				break;
 			case 2:
+				printAllStudent();
 				break;
 			case 3:
+				printOneStudent();
 				break;
 			case 4:
+				updateStudent();
 				break;
 			case 5:
+				deleteStudent();
 				break;
 			case 0:
 				System.out.println("시스템을 종료합니다.");
@@ -88,7 +108,122 @@ public class StudentController {
 		System.out.println("학생정보 등록 완료!");
 		
 	}//insertStudent() 종료
+	
+	//등록된 전체 학생정보를 출력하는 메소드
+	public void printAllStudent() {
+		System.out.println("\n---------- 전체 학생 정보 출력 ----------\n");
+		System.out.println("이름\t나이\t주소");
+		System.out.println("------------------------------");
+		//전체 학생정보는 students배열에 순서대로 저장되어있음
+		//-> 배열에 있는 정보를 출력
+		for(int i=0;i<index;i++) {
+			//students배열을 Student타입이 여러개 저장되어있는 배열
+			//students배열에서 한개의 데이터를 꺼내오면 -> Student타입
+			Student s = students[i];
+			//Student타입 객체에는 name,age,addr변수가 존재
+			//직접접근은 private로 막혀있어서 데이터를 가져오고싶을때는 해당 변수의 getter사용
+			System.out.println(s.getName()+"\t"+s.getAge()+"\t"+s.getAddr());
+		}
+	}//printAllStudent() 종료
+	
+	//학생 이름을 입력받아서 해당 학생 정보를 모두 출력하는 메소드
+	//입력받은 이름이 존재하지 않으면 "학생 정보를 조회할 수 없습니다."출력
+	public void printOneStudent() {
+		System.out.println("\n---------- 학생 정보 조회 ----------\n");
+		System.out.print("조회 할 학생 이름 입력 : ");
+		String name = sc.next();
+		int searchIndex = searchStudent(name);//여러곳에서 공통적으로 사용하는 기능은 메소드로 따로 만들어서 호출
+		if(searchIndex == -1) {
+			System.out.println("학생 정보를 조회할 수 없습니다.");
+		}else {
+			Student s = students[searchIndex];
+			System.out.println("학생 이름 : "+s.getName());
+			System.out.println("학생 나이 : "+s.getAge());
+			System.out.println("학생 주소 : "+s.getAddr());
+		}
+	}//printOneStudent()종료
+	
+	//학생이름을 입력받아서 해당학생이 존재하면 나이와 주소를 추가로 입력받아서 수정하는 메소드
+	public void updateStudent() {
+		System.out.println("\n-------- 회원 정보 수정 ----------\n");
+		System.out.print("수정 할 학생 이름 입력 : ");
+		String name = sc.next();
+		int searchIndex = searchStudent(name);
+		if(searchIndex == -1) {
+			System.out.println("학생 정보를 조회 할 수 없습니다.");
+		}else {
+			System.out.print("수정 할 학생 나이 입력 : ");
+			int age = sc.nextInt();
+			System.out.print("수정 할 학생 주소 입력 : ");
+			sc.nextLine();
+			String addr = sc.nextLine();
+			
+			//1) setter를 이용해서 객체에 저장되어있는 변수값을 수정
+			/*
+			Student s = students[searchIndex];
+			//Student의 각 변수는 모두 private로 변수에 직접접근불가 -> 값을 변경하고싶으면 -> setter메소드 사용
+			//s.age = age;
+			s.setAge(age);
+			s.setAddr(addr);
+			System.out.println("정보 수정 완료");
+			*/
+			//2) 새로운 Student객체를 만들어서 배열의 해당위치에 새로 주소를 대입
+			Student s = new Student(name,age,addr);
+			students[searchIndex] = s;
+			System.out.println("정보 수정 완료");
+		}
+	}//updateStudent() 종료
+	
+	//학생 이름을 입력받아서 해당 학생 정보를 배열에서 삭제
+	public void deleteStudent() {
+		System.out.println("\n---------- 학생 정보 삭제 ----------\n");
+		System.out.print("삭제 할 학생 이름 입력 : ");
+		String name = sc.next();
+		int searchIndex = searchStudent(name);
+		if(searchIndex == -1) {
+			System.out.println("학생 정보를 조회 할 수 없습니다.");
+		}else {
+			for(int i=searchIndex;i<index-1;i++) {
+				students[i] = students[i+1];
+			}
+			students[index-1] = null;
+			index--;
+			/*
+			index--;
+			students[index] = null;
+			
+			students[--index] = null;
+			 */
+			System.out.println("삭제 완료");
+		}
+		
+	}//deleteStudent() 종료
+	
+	
+	//이름을 매개변수로 받아서 해당이름의 학생이 배열의 몇번째에 있는지 되돌려주는 메소드
+	//일치하는 이름이 없으면 -1을 리턴
+	public int searchStudent(String name) {
+		int searchIndex = -1;
+		for(int i=0;i<index;i++) {
+			Student s = students[i];
+			String searchName = s.getName();
+			if(name.equals(searchName)) {
+				searchIndex = i;
+				break;
+			}
+		}
+		return searchIndex;
+	}//searchStudent() 종료
 }
+
+
+
+
+
+
+
+
+
 
 
 
